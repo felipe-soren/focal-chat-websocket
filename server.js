8274e09bffc1e09de19f0566101d66f752416dd1
unknown <Felipe Novaes>
2020-02-19 15:15:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+const rooms = { Política: { users:{} }, BBB: { users:{} }}
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+const port = process.env.PORT || 3030;
+
+server.listen(port)
+
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', (room, message) => {
+    console.log(rooms[room].users[socket.id])
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id]})
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      console.log(rooms)
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
+})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
